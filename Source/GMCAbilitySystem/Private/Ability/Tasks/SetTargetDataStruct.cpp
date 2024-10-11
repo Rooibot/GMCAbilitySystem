33751924c7a0031cfbf50a798d54145a10a979d2
Rachel Blackman <rachel.c.blackman@gmail.com>
2024-10-11 00:32:27 -0700

    Add a Set Target Data for instanced structs, since you can now make them in Blueprints.

---
+++ Source/GMCAbilitySystem/Private/Ability/Tasks/SetTargetDataStruct.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ability/Tasks/SetTargetDataStruct.h"
+
+UGMCAbilityTask_SetTargetDataStruct* UGMCAbilityTask_SetTargetDataStruct::SetTargetDataStruct(
+	UGMCAbility* OwningAbility, const FInstancedStruct& Target)
+{
+	UGMCAbilityTask_SetTargetDataStruct* NewTask = NewAbilityTask<UGMCAbilityTask_SetTargetDataStruct>(OwningAbility);
+	NewTask->Ability = OwningAbility;
+	NewTask->Target = Target;
+	return NewTask;
+}
+
+void UGMCAbilityTask_SetTargetDataStruct::ProgressTask(FInstancedStruct& TaskData)
+{
+	Super::ProgressTask(TaskData);
+	const FGMCAbilityTaskTargetDataStruct Data = TaskData.Get<FGMCAbilityTaskTargetDataStruct>();
+
+	Completed.Broadcast(Data.Target);
+	EndTask();
+	
+}
+
+void UGMCAbilityTask_SetTargetDataStruct::ClientProgressTask()
+{
+	FGMCAbilityTaskTargetDataStruct TaskData;
+	TaskData.TaskType = EGMCAbilityTaskDataType::Progress;
+	TaskData.AbilityID = Ability->GetAbilityID();
+	TaskData.TaskID = TaskID;
+	TaskData.Target = Target;
+	
+	const FInstancedStruct TaskDataInstance = FInstancedStruct::Make(TaskData);
+	Ability->OwnerAbilityComponent->QueueTaskData(TaskDataInstance);
+}
+
+void UGMCAbilityTask_SetTargetDataStruct::Activate()
+{
+	Super::Activate();
+
+	if (AbilitySystemComponent->GetNetMode() != NM_DedicatedServer)
+	{
+		ClientProgressTask();
+	}
+}
+
---
+++ Source/GMCAbilitySystem/Public/Ability/Tasks/SetTargetDataStruct.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GMCAbilityTaskBase.h"
+#include "SetTargetDataStruct.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FUGMCAbilityTaskTargetDataStructAsyncActionPin, FInstancedStruct, Target);
+
+USTRUCT(BlueprintType)
+struct FGMCAbilityTaskTargetDataStruct : public FGMCAbilityTaskData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, Category = "GMCAbilitySystem")
+	FInstancedStruct Target{nullptr};
+};
+
+/**
+ * 
+ */
+UCLASS()
+class GMCABILITYSYSTEM_API UGMCAbilityTask_SetTargetDataStruct : public UGMCAbilityTaskBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FUGMCAbilityTaskTargetDataStructAsyncActionPin	Completed;
+		
+	UPROPERTY(BlueprintReadWrite, Category = "GMCAbilitySystem")
+	FInstancedStruct Target{nullptr};
+		
+	virtual void ProgressTask(FInstancedStruct& TaskData) override;
+	virtual void ClientProgressTask() override;
+	 
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = "TRUE"), DisplayName="Set Target Data (Instanced Struct)",Category = "GMCAbilitySystem/Tasks")
+	static UGMCAbilityTask_SetTargetDataStruct* SetTargetDataStruct(UGMCAbility* OwningAbility, const FInstancedStruct& InstancedStruct);
+	 
+	//Overriding BP async action base
+	virtual void Activate() override;
+	
+};
